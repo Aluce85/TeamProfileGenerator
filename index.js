c21b647d3d92a8d3d2c117d77bba222517c5883b
Alex Luce <alexluce85@gmail.com>
2022-12-15 21:42:13 -0600

    added functions for employees

--- index.js
+++ index.js
@@ -37,92 +37,123 @@ function generateEmployee() {
 }
 
 function generateManager() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is your employee ID number?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email?',
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'What is your office location number?',
-        },
-    ]).then(response => {
-        let manager = new manager(response.name, response.id, response.email, response.officeNumber)
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your employee ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office location number?",
+      },
+    ])
+    .then((response) => {
+      let manager = new manager(
+        response.name,
+        response.id,
+        response.email,
+        response.officeNumber
+      );
 
-        employees.push(manager)
-        generateEmployee()
-    })
+      employees.push(manager);
+      generateEmployee();
+    });
 }
 
 function generateIntern() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is your employee ID number?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email?',
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: 'What college or university are you attending?',
-        },
-    ]).then(response => {
-        let intern = new intern(response.name, response.id, response.email, response.school)
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your employee ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What college or university are you attending?",
+      },
+    ])
+    .then((response) => {
+      let intern = new intern(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
 
-        employees.push(intern)
-        generateEmployee()
-    })
+      employees.push(intern);
+      generateEmployee();
+    });
 }
 
 function generateEngineer() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is your employee ID number?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email?',
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your GitHub Username?',
-        },
-    ]).then(response => {
-        let engineer = new engineer(response.name, response.id, response.email, response.github)
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your employee ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub Username?",
+      },
+    ])
+    .then((response) => {
+      let engineer = new engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
 
-        employees.push(engineer)
-        generateEmployee()
-    })
+      employees.push(engineer);
+      generateEmployee();
+    });
 }
 
+function makeHTML() {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(Employees), (err) =>
+    err ? console.log(err) : console.log("HTML has been generated")
+  );
+}
+
+generateEmployee();
