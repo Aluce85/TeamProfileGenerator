0140fb5b8adbe33d29779251a2d5296413d528dc
Alex Luce <alexluce85@gmail.com>
2022-12-15 21:13:22 -0600

    generate employee function added

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const inquirer = require("inquirer")
-const fs = require("fs")
-const manager = require("./lib/manager.js")
-const engineer = require(".lib/employee.js")
-const intern = require("./lib/intern")
-const employees = []
-const path = require("path")
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR,"generatedHTML.html")
-const render = require("./src/renderHTML")
+const inquirer = require("inquirer");
+const fs = require("fs");
+const manager = require("./lib/manager.js");
+const engineer = require(".lib/employee.js");
+const intern = require("./lib/intern");
+const employees = [];
+const path = require("path");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "generatedHTML.html");
+const render = require("./src/renderHTML");
+
+function generateEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "position",
+        message: "what is the name of your role?",
+        choices: ["manager", "intern", "engineer", "quit"],
+      },
+    ])
+    .then((answers) => {
+      if (answers.role === "Intern") {
+        console.log(answers);
+        generateIntern();
+      } else if (answers.role === "Manager") {
+        console.log(answers);
+        generateManager();
+      } else if (answers.role === "Engineer") {
+        console.log(answers);
+        generateEngineer();
+      } else {
+        console.log("hello");
+        makeHTML();
+      }
+    });
+}
